ebd5afaef443dd00797d8111c5e8075a222b0ab1
FredrikOl <102478913+FredrikOl@users.noreply.github.com>
2022-03-28 23:05:04 +0200

    Add files via upload

---
+++ franvaro.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){	
+	
+	$('#addFranvaro').click(function(){
+		let card = new Card(absences, absenceReasons);
+		$('#franvarolist').append(card.getElement());
+	});
+});
+ 
+class Card{
+	
+	constructor(exampleAbsences, exampleReasons){
+		this.exampleAbsences = exampleAbsences;
+		this.exampleReasons = exampleReasons;
+		this.randomizeData();
+	}
+	
+	getElement(){
+		let card = $("<div/>").addClass('franvaro');
+		let namn = $("<div/>").addClass('namn').html(this.namn).appendTo(card);
+		let datum = $("<div/>").addClass('datum').html(this.datum).appendTo(card);
+		let orsak = $("<div/>").addClass('orsak').html(this.orsak).appendTo(card);
+		return card;
+	}
+	
+	randomizeData = () => {
+		let randomData = this.exampleAbsences[
+			Math.floor(Math.random() * this.exampleAbsences.length)
+		];
+		this.namn = randomData.firstName + ' ' + randomData.lastName;
+		this.datum = this.getDateText(randomData.dateTimeFrom,randomData.dateTimeTo);
+		this.orsak = this.getOrsak(randomData.reasonGuid);
+	}
+	
+	getDateText = (from, tom) => {
+		 //Todo: Refaktorisera till datumobjekt
+		 if (!tom){
+			 return from.substr(0,10) + " - " + 'Tillsvidare';
+		 }
+		 if (from.substr(11) == '00:00:00' && tom.substr(11) == '23:59:59'){
+			if (from.substr(0,11) == tom.substr(0,11)){
+				return from.substr(0,11)
+			}
+			return from.substr(0,11) + " - " + tom.substr(0,11);
+		 }
+		 if (from.substr(0,11) == tom.substr(0,11)){
+			 return from.substr(0,16) + " - " + tom.substr(11);
+		 }
+		 return from.substr(0,16) + " - " + tom.substr(0,16);
+	 }
+	 
+	 getOrsak = (reasonGuid) => {
+		let orsak = this.exampleReasons.filter(
+			(x) => x.guid === reasonGuid
+		 );
+		 return orsak.length > 0 ? orsak[0].id : "";
+	 }
+}
